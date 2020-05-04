75aece1851d1cdcd98189a0877a074640cea4f6a
Rajat Singhal <rajatsinghal564@gmail.com>
2020-05-04 09:00:01 +0530

    First commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Tic-Tac-Toe
+
+Basic game using HTML, CSS, JS
+
+Improvements:
+
+- UI
+- Player vs Player
+- Smarter Computer Plays
---
+++ game.js
@@ -0,0 +1,115 @@
+var play_board = ["", "", "", "", "", "", "", "", ""];
+
+var player = "X";
+var computer = "O";
+
+var board_full = false;
+
+var board_container = document.querySelector(".play-area");
+var winner_statement = document.getElementById("winner");
+
+function render_board() {
+    board_container.innerHTML = "";
+    for(i=0; i<9; i++) {
+        board_container.innerHTML += `<div id="block_${i}" class="block" onclick="addPlayerMove(${i})"> ${play_board[i]} </div>`;
+    }
+}
+
+function addPlayerMove(i) {
+    if (!board_full && play_board[i]=="") {
+        play_board[i] = player;
+        update();
+        addComputerMove();
+    }
+}
+
+function addComputerMove() {
+    var selected = 0;
+    if (!board_full) {
+        do {
+            selected = Math.floor(Math.random() * 9);
+            // console.log(selected);
+        } while (play_board[selected]!="");
+
+        play_board[selected] = computer;
+        update();
+    }
+}
+
+function check_board_complete() {
+    var flag = true;
+    for(i=0; i<9; i++) {
+        if (play_board[i]=="") {
+            flag = false;
+            break;
+        }
+    }
+    // console.log(flag);
+    board_full = flag;
+}
+
+function update() {
+    render_board();
+    check_board_complete();
+    update_winner();
+}
+
+function check_line(a, b, c) {
+    return (
+        play_board[a] == play_board[b] &&
+        play_board[b] == play_board[c] &&
+        (play_board[a]==computer || play_board[a]==player)
+    );
+}
+
+function check_winner() {
+    // Check rows
+    for(i=0; i<3; i+=3) {
+        if(check_line(i, i+1, i+2)) {
+            return play_board[i];
+        }
+    }
+
+    // Check columns
+    for(i=0; i<3; i++) {
+        if(check_line(i, i+3, i+6)) {
+            return play_board[i];
+        }
+    }
+
+    // Check diag
+    if(check_line(0,4,8)) {
+        return play_board[0];
+    }
+    if(check_line(2,4,6)) {
+        return play_board[2];
+    }
+
+    return "";
+}
+
+function update_winner() {
+    var res = check_winner();
+    if(res == player) {
+        winner_statement.innerText = "Player Wins!";
+        board_full = true;
+    }
+    else if (res == computer) {
+        winner_statement.innerText = "Computer Wins";
+        board_full = true;
+    }
+    else if (board_full) {
+        winner_statement.innerText = "Draw";
+    }
+}
+
+function reset_board() {
+    for(i=0; i<9; i++) {
+        play_board[i] = "";
+    }
+    board_full = false;
+    winner_statement.innerText = "";
+    render_board();
+}
+
+render_board()
